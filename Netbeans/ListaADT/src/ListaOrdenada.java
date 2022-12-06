4c5488be817807edce8899d37d482a0773e20459
esandovalp <100039561+esandovalp@users.noreply.github.com>
2022-12-05 21:16:59 -0600

    agregue comentarios

--- Netbeans/ListaADT/src/ListaOrdenada.java
+++ Netbeans/ListaADT/src/ListaOrdenada.java
@@ -1,5 +1,6 @@
 
 public class ListaOrdenada <T extends Comparable<T>> extends Lista<T> implements ListaOrdenadaADT<T> {
+    // el orden es de mas chico a mas grande 
     
     public void agrega(T dato){
         NodoDoble<T> nuevo = new NodoDoble(dato);
@@ -8,20 +9,21 @@ public class ListaOrdenada <T extends Comparable<T>> extends Lista<T> implements
             primero = nuevo;
             ultimo = nuevo;
         } else 
-            if (primero.getDato().compareTo(dato) >= 0){
+            if (primero.getDato().compareTo(dato) >= 0){    // si el primero es mas grande que el nuevo
                 nuevo.setSiguiente(primero);    // no ocupa setAnterior(null) porque el constructor ya lo hace
-                primero.setAnterior(nuevo);
+                primero.setAnterior(nuevo);     // como el primero es mas grande, el nuevo va primero 
                 primero = nuevo;
             } else 
-                if (ultimo.getDato().compareTo(dato) <= 0){
-                    nuevo.setAnterior(ultimo);
+                if (ultimo.getDato().compareTo(dato) <= 0){ // si el ultimo es mas chico 
+                    nuevo.setAnterior(ultimo);  // el nuevo va al final 
                     ultimo.setSiguiente(nuevo);
                     ultimo = nuevo;
-                } else {
+                } else {        // busqueda para ver donde poner el dato 
                     NodoDoble<T> actual = primero.getSiguiente(); 
-                    while (actual.getDato().compareTo(dato) < 0)
-                        actual = actual.getSiguiente();
+                    while (actual.getDato().compareTo(dato) < 0)    // para hasta el actual sea mas grande, entonces el nuevo va antes del actual 
+                        actual = actual.getSiguiente();             // o sea, el nuevo va despues del punto donde para
                     
+                    // actualizacion de los nodos 
                     nuevo.setAnterior(actual.getAnterior());
                     nuevo.setSiguiente(actual);
                     actual.getAnterior().setSiguiente(nuevo);
@@ -104,5 +106,17 @@ public class ListaOrdenada <T extends Comparable<T>> extends Lista<T> implements
             }
         
         return resp; 
+    }
+    
+    public static void main(String[] args) {
+        ListaOrdenada<Integer> listaPrueba = new ListaOrdenada();
+        
+        listaPrueba.agrega(1);
+        listaPrueba.agrega(2);
+        listaPrueba.agrega(7);
+        listaPrueba.agrega(3);
+        
+        System.out.println(listaPrueba);
+        
     }
 }
--- Netbeans/ListaADT/src/Problema51.java
+++ Netbeans/ListaADT/src/Problema51.java
@@ -1,7 +1,16 @@
 
 public class Problema51 {
-//    public ListaOrdenada<T> ordenaPaises(ListaOrdenada<T> norte, ListaOrdenada<T> centro, ListaOrdenada<T> sur){
-//        
-//    }
+    /*
+    Escribe un programa que, usando las clases NodoSimple<T> y ListaOrdenada<T> sea capaz de almacenar (ordenados alfabéticamente)
+    los nombres de los países de América, según la ubicación: Norte, Centro y Sur, en tres listas distintas. 
+    Posteriormente, deberá formar una lista que contenga los nombres de todos los países del continente, también ordenados alfabéticamente. 
+    Las 3 listas iniciales no deben alterarse. Para formar la nueva lista se deben ir “mezclando” el contenido de las otras tres manteniendo el orden.
+    Modulariza y prueba tu solución. Los nombres de los países se deben leer de un archivo de texto.
+    */
+    ListaOrdenada<String> norte;
+    ListaOrdenada<String> centro;
+    ListaOrdenada<String> sur;
+    //ordenar segun la ubicacion usando nodos simples;
+    
     
 }
